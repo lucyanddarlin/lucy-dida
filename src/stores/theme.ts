848aa9442a0b8e9c4a0dcc186db1ec621a7809cb
lucy.li <lucy.li@quectel.com>
2023-09-02 10:46:51 +0800

    feat: 初始化 useThemeStore

---
+++ src/stores/theme.ts
@@ -0,0 +1,48 @@
+import { defineStore } from 'pinia'
+import { computed, ref } from 'vue'
+import { useDark, useToggle } from '@vueuse/core'
+import { darkTheme } from 'naive-ui'
+import { Theme, Themes } from '@/composables/settings/theme'
+
+export const useThemeStore = defineStore('themeStore', () => {
+  const isDark = useDark()
+  const toggleDark = useToggle(isDark)
+  const nativeTheme = computed(() => (isDark.value ? darkTheme : null))
+
+  const currentThemeName = isDark.value ? Theme.Dark : Theme.Light
+  const currentTheme = ref(
+    Themes.find((item) => item.name === currentThemeName)
+  )
+
+  /* pixel value */
+  const sideBarWidth = 48
+  const taskLeftViewPadding = 10
+
+  function changeTheme(theme: Theme) {
+    const themeItem = Themes.find((item) => item.name === theme)
+    if (themeItem) {
+      themeItem.handler()
+      currentTheme.value = themeItem
+    }
+  }
+
+  return {
+    isDark,
+    currentTheme,
+    toggleDark,
+    nativeTheme,
+    sideBarWidth,
+    taskLeftViewPadding,
+    changeTheme
+  }
+})
+
+let globalThemeStore: ReturnType<typeof useThemeStore>
+
+// To avoid using store before initializing Pinia.
+export const getGlobalThemeStore = () => {
+  if (!globalThemeStore) {
+    globalThemeStore = useThemeStore()
+  }
+  return globalThemeStore
+}
