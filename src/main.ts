d25bac79d056de949016979a775ff29a7574be7f
lucy.li <lucy.li@quectel.com>
2023-09-02 11:03:25 +0800

    fix: reslove navie and tailwind conflict

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,19 @@ import './assets/main.scss'
 import '@unocss/reset/tailwind.css'
 import 'uno.css'
 import router from './router'
+;(function setupApp() {
+  const app = createApp(App).use(createPinia())
+  app.use(router)
+  resolveNaiveAndTailWindConflict()
+  app.mount('#app')
+})()
 
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+// https://www.naiveui.com/zh-CN/os-theme/docs/style-conflict
+function resolveNaiveAndTailWindConflict() {
+  // We use tailwind reset as Unocss reset
+  // But some of reset style will replace some of naive=ui style
+  // To following the docs, we need to do this:
+  const meta = document.createElement('meta')
+  meta.name = 'naive-ui-style'
+  document.head.append(meta)
+}
